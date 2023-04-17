13eada698353a1e4d5d941c841176a2fc6bbe5e7
MD Jakariya Molla <121351153+jakariya-bd@users.noreply.github.com>
2023-04-17 20:01:44 +0600

    Update mm_fibo_0.c

--- Asked/mm_fibo_0.c
+++ Asked/mm_fibo_0.c
@@ -2,5 +2,25 @@
 
 int main(void)
 {
+  int row, a,b,c;
+  int i, k;
+  
+  scanf("%d", &row);
+  
+  //init part
+  a = 1;
+  b = 1;
+  c = 0;
+  
+  for(i=1; i <= row; i++){
+    for(k=1; k <= i; k++){
+      cout << a << " ";
+      
+      c = a + b;
+      a = b;
+      b = c;
+    }
+  }
+  
   return 0;
 }
